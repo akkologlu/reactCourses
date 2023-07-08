2685c9b4413a136632443606bc2d9e0af1cfe467
U190709067 <abdullahakkologlu@posta.mu.edu.tr>
2023-07-08 15:01:34 +0300

    react exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Courses from './Courses';
+import Loading from './Loading';
 function App() {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:3000/courses");
+      setCourses(response.data);
+      setLoading(false);
+    }
+    catch (error) {
+      setLoading(false);
+    }
+
+  }
+
+  const removeOneCourse = (id) => {
+    const updatedCourses = courses.filter((course) => course.id !== id);
+    setCourses(updatedCourses);
+  }
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading ? (
+          <Loading />
+        ) : (
+          <>
+            {courses.length === 0 ? (
+              <div className='refreshDiv'>
+                <h2>You deleted all courses!</h2>
+                <button onClick={fetchCourses} className='cardDeleteBtn'>Refresh</button>
+              </div>
+            ) : (<Courses courses={courses} removeOneCourse={removeOneCourse} />)}
+          </>
+        )
+      }
+
     </div>
   );
 }
